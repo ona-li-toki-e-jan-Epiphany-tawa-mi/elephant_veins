223e8e736f5fc166757f25b715038551c3936f08
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-12-28 16:30:43 -0500

    Fixed license header thingy

--- tools/types.lua
+++ tools/types.lua
@@ -1,19 +1,19 @@
--- This file is part of gigatools.
+-- This file is part of elephant_veins.
 --
 -- Copyright (c) 2024 ona-li-toki-e-jan-Epiphany-tawa-mi
 --
--- gigatools is free software: you can redistribute it and/or modify it under
--- the terms of the GNU Lesser General Public License as published by the Free
--- Software Foundation, either version 3 of the License, or (at your option) any
--- later version.
+-- elephant_veins is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as published by the
+-- Free Software Foundation, either version 3 of the License, or (at your
+-- option) any later version.
 --
--- gigatools is distributed in the hope that it will be useful, but WITHOUT ANY
--- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
--- A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+-- elephant_veins is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
 -- details.
 --
 -- You should have received a copy of the GNU Lesser General Public License
--- along with gigatools. If not, see <https://www.gnu.org/licenses/>.
+-- along with elephant_veins. If not, see <https://www.gnu.org/licenses/>.
 
 --- Common type aliases for lua-language-server static type checking.
 
